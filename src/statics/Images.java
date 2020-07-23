d11ba462150f094ae3e6611409f4416d4547f411
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2020-07-23 11:23:18 -0300

    :bug: Bug Fix

--- src/statics/Images.java
+++ src/statics/Images.java
@@ -3,7 +3,7 @@ package statics;
 public class Images {
 	
 	// My Images
-	public final static String anonymous = "assets/img/empresa/anonymous.png";
+	public final static String anonymous = "assets/img/anonymous/anonymous.png";
 	public final static String heart = "assets/img/heart/heart.png";
 	public final static String noHeart = "assets/img/heart/noheart.png";
 	public final static String floor = "assets/img/backgrounds/floor.png";
@@ -11,8 +11,8 @@ public class Images {
 	
 	// Icons
 	public final static String play = "assets/img/icons/play2.png";
-	public final static String ranking = "";
+	/*public final static String ranking = "";
 	public final static String exit = "";
-	public final static String settings = "";
+	public final static String settings = "";*/
 
 }
